7dbd0f973b65ca5f5c054e7173e358bdeec7b162
firewaterge <gyczt879574764@gmail.com>
2017-05-01 07:50:20 -0700

    Add files via upload

---
+++ app/sign_module/lib/Sign.js
@@ -0,0 +1,20 @@
+module.exports = start;
+
+function start(config){
+    var app = config.app;
+
+    app.get('/signPage', function(req, res){
+        console.log('signPage');
+        res.send('hehe');
+    });
+
+    return (new test());
+};
+
+function test(){
+    this.a = '23';
+}
+
+test.prototype.hehe = function(){
+    console.log('asdf');
+}
