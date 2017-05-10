1a1e95437286f73f8e9d6d55f88e3e7941727c1c
firewaterge <gyczt879574764@gmail.com>
2017-05-09 21:56:24 -0700

    add saferman module

---
+++ app/saferman/lib/main.js
@@ -0,0 +1,32 @@
+module.exports = start;
+
+function start(config){
+    var app = config.app;
+
+    return (new API(config));
+};
+
+function API(config){
+    this.config = config;
+}
+
+//exec callback function
+API.prototype.sql = function(ID,right,callback){
+    this.config.connection.query('SELECT * FROM RightsTable',
+        function (error, results, fields){
+            if (error) throw error;
+
+            var value;
+
+            if(results[0] != undefined){
+                var match = new RegExp(right);
+                value = (match.exec(results[0].Rights)!=null);
+
+                callback(value);
+            }else{
+                value = false;
+
+                callback(value);
+            }
+        });
+}
