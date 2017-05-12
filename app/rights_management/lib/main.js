c1d004b0dd847afc0ba7819d608e8274d4a25eb7
firewaterge <gyczt879574764@gmail.com>
2017-05-12 06:21:16 -0700

    update rights_management

--- app/rights_management/lib/main.js
+++ app/rights_management/lib/main.js
@@ -34,6 +34,31 @@ API.prototype.isAvailable = function(ID,right,callback){
 }
 
 API.prototype.Add = function(ID,right){
+
+    //check rights string
+
+    //get rights string
+    var sql_string = 'SELECT * FROM RightsTable WHERE ID=' + ID;
+
+    this.config.modules['saferman'].sql(sql_string,function(results){
+        var value;
+
+        if(results[0] != undefined){
+            var match = new RegExp(right);
+            value = (match.exec(results[0].Rights)!=null);
+
+            if(callback!=undefined)
+                callback(value);
+        }else{
+            value = false;
+
+
+            if(callback!=undefined)
+                callback(value);
+        }
+    });
+
+    //update rights string
     this.config.modules['saferman'].sql("UPDATE RightsTable SET rights='write' WHERE ID=0");
 }
 
