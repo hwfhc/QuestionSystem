90d2e2e4834cc3b08a9bf44dc24a700766a9d97a
firewaterge <gyczt879574764@gmail.com>
2017-05-17 02:22:17 -0700

    add personalinformation_module

---
+++ app/personalinformation_module/lib/main.js
@@ -0,0 +1,16 @@
+module.exports = start;
+
+function start(config){
+    var app = config.app;
+
+    return (new API(config));
+};
+
+function API(config){
+    this.config = config;
+}
+
+//exec callback function
+API.prototype.isAvailable = require('./isAvailable.js');
+
+API.prototype.Add = require('./Add.js');
