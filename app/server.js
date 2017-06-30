b03e382f6b87e1a66b9aea4b0d30b5a8fa962c7e
firewaterge <gyczt879574764@gmail.com>
2017-06-30 06:19:01 -0700

    update code of server.js

--- app/server.js
+++ app/server.js
@@ -10,28 +10,34 @@ var config = {
     modules: []
 };
 
-
-//init common route
-config.app.get('/css/:file',function(req,res){
-    console.log(directory + '/css/' + req.params['file']);
-    res.sendFile(directory + '/css/' + req.params['file']);
-});
-
-config.app.get('/javascripts/:file',function(req,res){
-    res.sendFile(root + '/javascripts/' + req.params['file']);
-});
-
-
-
-//module load area
-config.modules['sign_module'] = (require('sign_module'))(config);
-config.modules['rights_management'] = (require('rights_management'))(config);
-config.modules['personalinformation_module'] = (require('personalinformation_module'))(config);
-config.modules['saferman'] = (require('saferman'))(config);
-
-config.modules['personalinformation_module'].initUser('fea');
+commonRouteInit();
+moduleInit();
 
 //start server, and listen to port
 var server = config.app.listen(8080,function(){
     console.log('server start...');
 });
+
+
+
+function commonRouteInit(){
+
+    config.app.get('/css/:file',function(req,res){
+        console.log(directory + '/css/' + req.params['file']);
+        res.sendFile(directory + '/css/' + req.params['file']);
+    });
+
+    config.app.get('/javascripts/:file',function(req,res){
+        res.sendFile(root + '/javascripts/' + req.params['file']);
+    });
+}
+
+function moduleInit(){
+
+    config.modules['sign_module'] = (require('sign_module'))(config);
+    config.modules['rights_management'] = (require('rights_management'))(config);
+    config.modules['personalinformation_module'] = (require('personalinformation_module'))(config);
+    config.modules['saferman'] = (require('saferman'))(config);
+}
+
+
