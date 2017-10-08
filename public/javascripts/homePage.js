88e36dc9326b2495294772be8bc5c729723f032c
hwfhc <gyczt879574764@gmail.com>
2017-10-08 03:00:48 -0700

    update drypot

--- public/javascripts/homePage.js
+++ public/javascripts/homePage.js
@@ -1,15 +1,3 @@
-function getPersonalInformation(){
-    var username = document.getElementById('username');
-    var xhttp = new XMLHttpRequest();
+var demo = window.location.pathname;
 
-    xhttp.onreadystatechange = function(){
-        if(this.readyState == 4 && this.status == 200){
-            username.innerHTML = JSON.parse(this.responseText).username;
-        }
-    }
-
-    xhttp.open('GET','/personalHomePage/getPersonalInformation',true);
-    xhttp.send();
-}
-
-getPersonalInformation();
+console.log(demo);
