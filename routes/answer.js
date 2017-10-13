a297f0e86765ea7214b3a5d4ccc8fe8f9726fbf0
hwfhc <gyczt879574764@gmail.com>
2017-10-12 23:31:26 -0700

    debug

--- routes/answer.js
+++ routes/answer.js
@@ -3,50 +3,131 @@ module.exports = init;
 const sign_module = require('../bin/sign_module');
 
 function init(app,directory){
-    app.get('/signPage',function(req,res){
-        console.log('asdf');
-        sign_module.logOut(req);
-        res.sendFile('signPage.html');
+    app.get('/answer/list/:questionID',function(req,res){
+        let questionID = req.param['questionID'];
+
+
+        let authorID;
+        let userID = getUserID(req);
+
+        view_module.getQuestionDetail(questionID,function(result){
+            authorID = result.authorID;
+
+            if(userID == authorID && questionID){
+                view_module.getAnswerList(req.session.questionID,function(result){
+                    res.send(JSON.stringify(result));
+                })
+            }
+        });
     });
 
 
-    app.get('/homePage',function(req,res){
-        var id = getUserID(req);
-        res.redirect(`/homePage/${id}`)
-    });
-
-    app.get('/homePage/:id',function(req,res){
-        res.sendFile('homePage.html');
-    });
-
-
-    app.get('/publishPage',function(req,res){
-        res.sendFile('publishPage.html');
-    });
-
-    app.get('/answerPage',function(req,res){
-        res.sendFile('answerPage.html');
-    });
-
-    app.get('/answerDetail/:answerID', function(req, res){
-        let answerID = getAnswerID(req);
+    app.get('/answer/:answerID/detail',function(req,res){
+        let dataToSended = {};
+        let userIDofAnswer;
+        let answerID = req.param['answerID'];
+        let questionID;
 
         if(!answerID){
-            res.redirect('/personalHomePage');
+            return;
+        }
+
+        let answerDetail = new Promise(function(resolve,reject){
+            view_module.getAnswerAndUserIDbyID(answerID,function(results){
+                dataToSended.answer = results.answer;
+                dataToSended.score = results.score;
+
+                questionID = results.questionID;
+                userIDofAnswer = results.userID;
+                resolve();
+            });
+        });
+
+        answerDetail.then(function(){
+
+            let getUsername = new Promise(function(resolve,reject){
+                //console.log('userIDofAnswer is: '+userIDofAnswer);
+                personalinformation_module.getUsernameByID(userIDofAnswer,function(result){
+                    dataToSended.username = result;
+                    resolve();
+                });
+            });
+
+            let getQuestionDetail = new Promise(function(resolve,reject){
+                //console.log('questionID is: '+questionID);
+                view_module.getQuestionDetail(questionID,function(result){
+                    dataToSended.title = result.title;
+                    dataToSended.description = result.description;
+                    resolve();
+                });
+            });
+
+            Promise.all([
+                getUsername,
+                getQuestionDetail
+            ]).then(function(){
+                res.send(JSON.stringify(dataToSended));
+            })
+
+        });
+
+    });
+
+    app.post('/answer/publish', function(req, res){
+        var answer = getAnswer();
+        var questionID = getQuestionID();
+        var userID = getUserID();
+
+        if(answer && questionID && userID){
+            answer_module.answerAskQuestion(answer,questionID,userID,function(){
+                res.redirect('/signInSuccess');
+            });
         }else{
-            req.session.answerID = answerID;
-            res.sendFile('answerDetail.html');
+            res.redirect('/personalHomePage');
+        }
+
+
+        function getAnswer(){
+            return req.body.answer;
+        }
+
+        function getQuestionID(){
+            return req.session.questionID
+        }
+
+        function getUserID(){
+            return req.session.ID;
         }
 
     });
 
-    app.get('/signInSuccess',function(req,res){
+    app.get('/answer/:id/author',function(req,res){
         res.sendFile('signInSuccess.html');
     });
 
-    app.get('/signInFail',function(req,res){
-        res.sendFile('signInFail.html');
+    app.post('/answer/:id/setScore',function(req,res){
+        let score = getScore();
+        let answerID = getAnswerID();
+
+        if(score && answerID){
+            answer_module.setScoreByID(answerID,score,function(){
+                res.redirect('/signInSuccess');
+            });
+        }else{
+            res.redirect('/personalHomePage');
+        }
+
+        function getScore(){
+            return req.body.score;
+        }
+
+        function getAnswerID(){
+            return req.session.answerID;
+        }
+
+
     });
+
 }
 
 function getUserID(req){
--- routes/question.js
+++ routes/question.js
@@ -3,30 +3,71 @@ module.exports = init;
 const sign_module = require('../bin/sign_module');
 
 function init(app,directory){
-    app.get('/signPage',function(req,res){
-        console.log('asdf');
-        sign_module.logOut(req);
-        res.sendFile('signPage.html');
-    });
-
     app.get('/question/list',function(req,res){
-        res.sendFile('questionList.html');
-    });
-    app.get('/question/:id/detail',function(req,res){
-		
-    });
-    app.get('/question/:id/title',function(req,res){
-		
-    });
-    app.get('/question/:author',function(req,res){
-		
-    });
-    app.get('/question/public',function(req,res){
-		
+        view_module.getQuestionList(function(results){
+            res.send(JSON.stringify(results));
+        })
     });
 
-	
-	
+    app.get('/question/:questionID/detail',function(req,res){
+        let dataToSended = {};
+
+        let questionID = req.param['questionID'];
+        let authorID;
+        let userID = getUserID();
+
+        if(!questionID){
+            res.redirect('/personalHomePage');
+            return;
+        }
+
+        view_module.getQuestionDetail(questionID,function(result){
+            dataToSended.title = result.title;
+            dataToSended.description = result.description;
+            dataToSended.total_score = result.total_score;
+
+            authorID = result.authorID;
+
+            personalinformation_module.getUsernameByID(authorID,function(result){
+                dataToSended.author_name = result;
+
+                view_module.getScoreByUserID(userID,questionID,function(result){
+                    dataToSended.score = result.score;
+                    dataToSended.answer = result.answer;
+
+                    if(userID==authorID){
+                        dataToSended.isAuthor = true;
+                    }else{
+                        dataToSended.isAuthor = false;
+                    }
+
+                    res.send(JSON.stringify(dataToSended));
+                });
+            });
+        });
+    });
+
+    app.get('/question/:id/title',function(req,res){
+
+    });
+
+    app.get('/question/:id/author',function(req,res){
+
+    });
+
+    app.post('/question/publish',function(req,res){
+        var userID = getUserID(req);
+        var title = req.body.title;
+        var description = req.body.description;
+
+        publish_module.publishAskQuestion(title,description,5,req.session.ID,function (){
+            res.redirect('/signInSuccess');
+        });
+    });
+}
+
+
+
 function getUserID(req){
     return req.session.ID;
 }
